421dbff3f712f850753970306dcfc2ebfdb2cb74
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-06-28 13:20:21 +0300

    extended list

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = []
+    if length == 0:
+        print(fib_list)
+    elif length == 1:
+        fib_list.append(0)
+        print(fib_list)
+    elif length > 1:
+        fib_list.append(0)
+        i = 2
+        while i <= length:
+            fib_list.append(i - 1)
+            updated_list = fib_list
+            
+            #fib_list.append((fib_list[i] + fib_list[i-1]))
+            print(fib_list)
+            i += 1
